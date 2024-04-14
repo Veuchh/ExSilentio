02851c4e9c3d48333010b7ee969ea6c4401d8836
FaerDerr <matheo.souville@gmail.com>
2024-04-14 12:04:57 +0200

    updated valid prompts

--- Assets/Core/Data/Scripts/WordID.cs
+++ Assets/Core/Data/Scripts/WordID.cs
@@ -2,6 +2,6 @@ namespace LW.Data
 {
     public enum WordID
     {
-       test, test_additional, test_semanticallyClose, building, train, boat, sun, ocean, fountain, seagul, wind, fish, railway_station
+       test, test_additional, test_semanticallyClose, building, train, boat, sun, ocean, fountain, seagul, wind, fish, railway_station, house, city, port, town, habitation, sea
     }
 }
