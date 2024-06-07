d3af0b75dafd803ab195cec80e42a1ec6808cd47
VOGEL Louis <louis.vogel.auditeur@lecnam.net>
2024-06-07 16:52:27 +0200

    Add boundaries to ocean city

--- Assets/Core/Data/Scripts/StaticData.cs
+++ Assets/Core/Data/Scripts/StaticData.cs
@@ -1,8 +1,12 @@
+using UnityEngine;
+
 namespace LW.Data
 {
     public static class StaticData
     {
         private static int openWindowsAmount = 0;
+        private static Transform playerTransform;
         public static int OpenWindowsAmount { get => openWindowsAmount; set => openWindowsAmount = value; }
+        public static Transform PlayerTransform { get => playerTransform; set => playerTransform = value; }
     }
 }
--- Assets/Core/Player/Scripts/SpawnPoint.cs
+++ Assets/Core/Player/Scripts/SpawnPoint.cs
@@ -1,53 +1,59 @@
+using LW.Data;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-[SelectionBase]
-public class SpawnPoint : MonoBehaviour
+namespace LW.Player
 {
-    [Flags]
-    enum ComponentsToAddOnSpawn
+    [SelectionBase]
+    public class SpawnPoint : MonoBehaviour
     {
-        ReflectionHandler = 2,
-        DistanceToWater = 3,
-    }
-
-    [SerializeField] GameObject playerPrefab;
-    [SerializeField] List<GameObject> instantiateAsPlayerChild;
-    [SerializeField] List<GameObject> setAsPlayerChild;
-    [SerializeField] List<MeshRenderer> waterPlanes;
-    [SerializeField] ComponentsToAddOnSpawn componentsToAddToPlayer;
-
-    private void Start()
-    {
-        GameObject player = Instantiate(playerPrefab, transform.position, transform.rotation);
-
-        foreach (ComponentsToAddOnSpawn componentType in Enum.GetValues(typeof(ComponentsToAddOnSpawn)))
+        [Flags]
+        enum ComponentsToAddOnSpawn
         {
-            if (componentsToAddToPlayer.HasFlag(componentType))
-                switch (componentType)
-                {
-                    case ComponentsToAddOnSpawn.ReflectionHandler:
-                        player.AddComponent<ReflectionHandler>().Init(waterPlanes);
-                        break;
-                    case ComponentsToAddOnSpawn.DistanceToWater:
-                        player.AddComponent<WwiseDistanceToWater>().Init(0, 35);
-                        break;
-                }
+            ReflectionHandler = 2,
+            DistanceToWater = 3,
         }
 
-        foreach (GameObject prefab in instantiateAsPlayerChild)
-        {
-            Instantiate(prefab, player.transform.position, player.transform.rotation, player.transform);
-        }
+        [SerializeField] GameObject playerPrefab;
+        [SerializeField] List<GameObject> instantiateAsPlayerChild;
+        [SerializeField] List<GameObject> setAsPlayerChild;
+        [SerializeField] List<MeshRenderer> waterPlanes;
+        [SerializeField] ComponentsToAddOnSpawn componentsToAddToPlayer;
 
-        foreach (GameObject go in setAsPlayerChild)
+        private void Start()
         {
-           go.transform.parent = player.transform;
-           go.transform.localPosition = Vector3.zero;
-           go.transform.localRotation = Quaternion.identity;
-        }
+            GameObject player = Instantiate(playerPrefab, transform.position, transform.rotation);
 
-        Destroy(gameObject);
+            StaticData.PlayerTransform = player.transform;
+
+            foreach (ComponentsToAddOnSpawn componentType in Enum.GetValues(typeof(ComponentsToAddOnSpawn)))
+            {
+                if (componentsToAddToPlayer.HasFlag(componentType))
+                    switch (componentType)
+                    {
+                        case ComponentsToAddOnSpawn.ReflectionHandler:
+                            player.AddComponent<ReflectionHandler>().Init(waterPlanes);
+                            break;
+                        case ComponentsToAddOnSpawn.DistanceToWater:
+                            player.AddComponent<WwiseDistanceToWater>().Init(0, 35);
+                            break;
+                    }
+            }
+
+            foreach (GameObject prefab in instantiateAsPlayerChild)
+            {
+                Instantiate(prefab, player.transform.position, player.transform.rotation, player.transform);
+            }
+
+            foreach (GameObject go in setAsPlayerChild)
+            {
+                go.transform.parent = player.transform;
+                go.transform.localPosition = Vector3.zero;
+                go.transform.localRotation = Quaternion.identity;
+            }
+
+            Destroy(gameObject);
+        }
     }
 }
